ac5138326e075aa964384e9da95d291f2453fff8
Jorge Eder <jorgeeder.js@gmail.com>
2022-04-08 10:40:09 -0300

    Checking for Duplicate User

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -6,6 +6,8 @@ import GithubProvider from 'next-auth/providers/github';
 import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
+  secret: process.env.NEXTAUTH_SECRET,
+
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_CLIENT_ID,
@@ -19,14 +21,31 @@ export default NextAuth({
 
       try {
         await fauna.query(
-          q.Create(
-            q.Collection('users'),
-            { data: { email } }
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { email } }
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email)
+              )
+            )
           )
         )
 
         return true
-      } catch {
+      } catch (err) {
+        console.log(err)
         return false
       }
     },
